e8a3e3ffc187e0d908e5aaf7a4a4c2c6e6da554c
Vargentum <vargentum@gmail.com>
2016-03-28 23:06:15 +0300

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, {PropTypes} from 'react';
+
+class App extends React.Component {
+  static propTypes = {}
+
+  // state = {}
+  // methodName = () =>
+  //   <div></div>
+    
+  render() {
+    const {
+
+    } = this.props
+
+    return (
+      <div>App</div>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    devtool: 'eval',
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?/,
+                loaders: ['babel'],
+                include: path.join(__dirname, 'src')
+            }
+        ]
+    }
+}
